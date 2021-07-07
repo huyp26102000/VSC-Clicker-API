fdda84aed044668832e3bf322dfa9808ed95b40f
huyp26102000 <huyp26102000@gmail.com>
2021-07-07 13:06:27 +0700

    update package

--- package.py
+++ package.py
@@ -1,19 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, session, abort
 
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import Flow
-from pip._vendor import cachecontrol
-import google.auth.transport.requests
-
-import pathlib
-import os
-import json
-import sys
-import glob
-import serial
-import time, datetime
-import uuid
-
 import firebase_admin
 from firebase_admin import credentials, firestore
 from firebase_admin import credentials, db
